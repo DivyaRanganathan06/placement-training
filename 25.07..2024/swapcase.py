5258ebc525e70b254178d36ae56f5c002bd98bb0
DivyaRanganathan06 <168944450+DivyaRanganathan06@users.noreply.github.com>
2024-07-25 21:23:43 +0530

    Create swapcase.py

---
+++ 25.07..2024/swapcase.py
@@ -0,0 +1,7 @@
+def swap_case(input_string):
+    return input_string.swapcase()
+
+# Example usage:
+original_string = "Hello, World!"
+swapped_string = swap_case(original_string)
+print(swapped_string)  # Output: hELLO, wORLD!
