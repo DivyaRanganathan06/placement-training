097a68b26594a2a16a4dd60c5453f7407fcf7d20
DivyaRanganathan06 <168944450+DivyaRanganathan06@users.noreply.github.com>
2024-07-25 16:35:30 +0530

    for_loop.py

---
+++ 24.07.2024/for_loop.py
@@ -0,0 +1,7 @@
+#The included code stub will read an integer, , from STDIN.
+#Without using any string methods, try to print the following:
+#Note that "" represents the consecutive values in between.
+
+n = int(input())
+for i in range(n):
+    print(i+1,end='')
